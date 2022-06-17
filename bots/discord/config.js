33f4275910fac97f4c9e5cf0569cf2f007bf495c
spiderthehacker <48144985+spiderthehacker@users.noreply.github.com>
2022-06-17 20:11:58 +0545

    Update config.js

--- bots/discord/config.js
+++ bots/discord/config.js
@@ -2,7 +2,7 @@ module.exports = {
     apiurl: 'http://localhost:1337',
     apipassword: 'spider',
 
-    discordtoken: 'OTg3MzM4NzkxODQyODc3NDUw.GFDq1q.LHiIKZZxpfs-VqwmjtRxa_J3wp49CHzdMyw1Rs',
+    discordtoken: '',
     discordprefix: '!',
     secretpassword: 'spider',
     botuser_rolename: 'Bot User',
